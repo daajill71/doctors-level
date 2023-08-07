46e9e9ba05fc07e8d8b89c365679735e0ca3c4bf
Diana Jill <daajill71@gmail.com>
2023-08-07 14:18:08 +0300

    edited jobs.js routes

--- doctors-backend/routes/jobs.js
+++ doctors-backend/routes/jobs.js
@@ -2,9 +2,21 @@ const express = require('express');
 const router = express.Router();
 const Job = require('../models/job'); // Import your Mongoose model
 
+router.get('/', async (req, res) => {
+  try {
+    const jobs = await Job.find();
+    res.json(jobs);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error fetching jobs' });
+  }
+});
+
+module.exports = router;
+
 
 // Sample job data (Replace this with your actual job data)
-// const jobsData = [
+//const jobsData = [
   //{ id: 1, title: 'Software Engineer', compensation: '100,000' },
   //{ id: 2, title: 'Data Scientist', compensation: '90,000' },
   //{ id: 3, title: 'Product Manager', compensation: '110,000' },
@@ -12,17 +24,7 @@ const Job = require('../models/job'); // Import your Mongoose model
 //];
 
 // Route to fetch jobs
-router.get('/', async (req, res) => {
-    try {
-      const jobs = await Job.find();
-      res.json(jobs);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'Error fetching jobs' });
-    }
-  });
-  
-  module.exports = router;
+
   // Route to handle GET requests for /api/jobs
 // router.get('/', (req, res) => {
  // res.json(jobsData);
